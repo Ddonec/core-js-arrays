1fd15aa2bc9635824fefe452a3bcf61eba68c208
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-09 01:09:57 +0300

    feat: complete getIdentityMatrix task, now need complete last

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -420,8 +420,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, row) => {
+    return Array.from({ length: n }, (__, column) => {
+      return row === column ? 1 : 0;
+    });
+  });
 }
 
 /**
